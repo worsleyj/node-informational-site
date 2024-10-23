609ac8d9bfb2745a6edf88b43010da67430badb0
worsleyj <josephworsley23@gmail.com>
2024-10-23 15:33:39 -0400

    add dynamic url parsing for requests

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 const port = 8080;
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("index.html", function (error, data) {
+  const q = url.parse(req.url, true);
+  const filename = "." + q.pathname;
+
+  fs.readFile(filename, function (error, data) {
     if (error) {
       res.writeHead(404);
-      res.write("error: file not found");
+      res.end("error: file not found");
     } else {
-      res.write(data);
-      res.end();
+      res.writeHead(200, { "Content-Type": "text/html" });
+      //   res.write(data);
+      res.end(data);
     }
   });
 });
