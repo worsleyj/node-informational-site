070baaff004f9a936e6b038b7ea3dc05dab16e98
worsleyj <josephworsley23@gmail.com>
2024-10-23 15:42:21 -0400

    hook up 404 page and index page

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ const port = 8080;
 
 const server = http.createServer(function (req, res) {
   const q = url.parse(req.url, true);
-  const filename = "." + q.pathname;
+  let filename = "";
+  if (q.pathname === "/") {
+    filename = "./index.html";
+  } else {
+    filename = "." + q.pathname;
+  }
 
   fs.readFile(filename, function (error, data) {
     if (error) {
       res.writeHead(404);
-      res.end("error: file not found");
+      res.end(
+        fs.readFileSync("404.html", "utf-8", (err, data) => {
+          if (err) throw err;
+          return data;
+        })
+      );
     } else {
       res.writeHead(200, { "Content-Type": "text/html" });
       //   res.write(data);
