9e25c4f3feac510776aca3a1b2187a021e0ed88c
worsleyj <josephworsley23@gmail.com>
2024-10-28 16:13:43 -0400

    fix non index routing and add 404 routing

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-app.get("/about", (req, res) =>
+app.get("/about*", (req, res) =>
   res.sendFile(path.join(__dirname, "/about.html"))
 );
-app.get("/contact-me", (req, res) =>
+app.get("/contact-me*", (req, res) =>
   res.sendFile(path.join(__dirname, "/contact-me.html"))
 );
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/404.html")));
 
 app.listen(PORT, () => {
   console.log(`My first express app ~ listening on port ${PORT}!`);
